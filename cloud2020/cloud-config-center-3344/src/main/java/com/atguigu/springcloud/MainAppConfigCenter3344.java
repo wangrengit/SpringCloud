0dc6676438aefc493ce071b63fe7ea25b0399a8b
wang_glb <819278056_glb@qq.com>
2020-09-13 15:39:02 +0800

    2020/09/13第一次创建

---
+++ cloud2020/cloud-config-center-3344/src/main/java/com/atguigu/springcloud/MainAppConfigCenter3344.java
@@ -0,0 +1,16 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableConfigServer
+public class MainAppConfigCenter3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(MainAppConfigCenter3344.class,args);
+    }
+}
