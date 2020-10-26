b8ef4635f665f3e39b2f2f7585d356f0d448693f
wang_glb <819278056_glb@qq.com>
2020-10-26 15:05:13 +0800

    2020-10-26 lanuch2

---
+++ boot-lanuch2/src/main/java/com/example/demo/BootLanuch2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootLanuch2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootLanuch2Application.class, args);
+	}
+
+}
